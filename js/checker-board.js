9a6b599b0380d53cef6d70471b29d740444b6c56
doomium-chloride <47913762+doomium-chloride@users.noreply.github.com>
2020-08-24 11:59:40 +1200

    Add dummy info to checker pager

--- js/checker-board.js
+++ js/checker-board.js
@@ -1,5 +1,6 @@
 
 
+
 function toggleMenu(menu){
     let visibility = menu.style.visibility;
     if(visibility == 'hidden'){
@@ -12,5 +13,29 @@ function initCheckerBoard(){
     let menu = document.getElementById('dropdown-menu');
     let menuButton = document.getElementById('dropdown-menu-button');
     menuButton.addEventListener('click', () => toggleMenu(menu));
+
+    let button1 = document.getElementById('button1');
+    let button2 = document.getElementById('button2');
+    let button3 = document.getElementById('button3');
+
+    let checkerPic1 = document.getElementById('checker-pic-1');
+    let checkerPic2 = document.getElementById('checker-pic-2');
+    let checkerPic3 = document.getElementById('checker-pic-3');
+
+    let checkerPics = [checkerPic1, checkerPic2, checkerPic3];
+
+    let score1 = document.getElementById('checker-score-1');
+    let score2 = document.getElementById('checker-score-2');
+    let score3 = document.getElementById('checker-score-3');
+
+    let scores = [score1, score2, score3];
+
+    button1.addEventListener('click', () => makeImageActive(checkerPic1, checkerPics));
+    button2.addEventListener('click', () => makeImageActive(checkerPic2, checkerPics));
+    button3.addEventListener('click', () => makeImageActive(checkerPic3, checkerPics));
+
+    button1.addEventListener('click', () => makeImageActive(score1, scores));
+    button2.addEventListener('click', () => makeImageActive(score2, scores));
+    button3.addEventListener('click', () => makeImageActive(score3, scores));
 }
 
