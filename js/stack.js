b1cde81c776d2bdd2b2cbfceadb68ef3909fd499
doomium-chloride <47913762+doomium-chloride@users.noreply.github.com>
2020-08-31 14:02:03 +1200

    Add dummy graph using chart.js

--- js/stack.js
+++ js/stack.js
@@ -5,7 +5,7 @@ function translateX(value, units = "px"){
 
 function verticalHandler(value, span, slider){
     span.innerText = value;
-    slider.style.transform = translateX(value);
+    slider.style.transform = translateX(value * 300, "%");
 }
 
 
@@ -15,5 +15,29 @@ function initStack(){
     let verticalSpan = document.getElementById('vertical-value');
 
     verticalSlider.addEventListener('input', (e) => verticalHandler(e.target.value, verticalSpan, graphSlider));
+
+    let ctx = document.getElementById('stack-chart').getContext('2d');
+    console.log(ctx)
+
+    let chart = new Chart(ctx, {
+        // The type of chart we want to create
+        type: 'line',
+    
+        // The data for our dataset
+        data: {
+            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            datasets: [{
+                label: 'My First dataset',
+                backgroundColor: 'rgb(255, 99, 132)',
+                borderColor: 'rgb(255, 99, 132)',
+                data: [0, 10, 5, 2, 20, 30, 45]
+            }]
+        },
+    
+        // Configuration options go here
+        options: {}
+    });
+
+    
 
 }
