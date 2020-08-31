c53da255936928e01a3ba1e8a20401f4986dae10
doomium-chloride <47913762+doomium-chloride@users.noreply.github.com>
2020-08-31 19:03:46 +1200

    Attempt to calibrate dummy graph

--- js/stack.js
+++ js/stack.js
@@ -3,9 +3,18 @@ function translateX(value, units = "px"){
     return "translateX(" + value + units + ")";
 }
 
-function graphHandler(value, span, slider){
+function adjustLine(value, offset, maxLen){
+    return (value + offset) / (maxLen);
+}
+
+function graphHandler(value, span, slider, container, chart){
     span.innerText = value;
-    slider.style.transform = translateX(value * 300, "%");
+    let width = container.offsetWidth;
+    slider.style.height = container.offsetHeight + "px";
+    value = parseInt(value);
+    let percent = adjustLine(value, -100, (width+20) / width * 100);
+    slider.style.transform = translateX(percent * width, "px");
+    console.log(chart.options.scales.yAxes[0])
 }
 
 
@@ -13,8 +22,10 @@ function initStack(){
     let graphSlider = document.getElementById('graph-slider');
     let verticalSlider = document.getElementById('vertical-slider');
     let verticalSpan = document.getElementById('vertical-value');
+    let graphContainer = document.getElementById('graph');
 
-    verticalSlider.addEventListener('input', (e) => graphHandler(e.target.value, verticalSpan, graphSlider));
+    
+
 
     let ctx = document.getElementById('stack-chart').getContext('2d');
     console.log(ctx)
@@ -25,18 +36,33 @@ function initStack(){
     
         // The data for our dataset
         data: {
-            labels: [0, 1, 2, 3, 4, 5, 6],
+            labels: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
             datasets: [{
-                label: 'My First dataset',
-                backgroundColor: 'rgb(255, 99, 132)',
+                label: 'Dummy dataset',
                 borderColor: 'rgb(255, 99, 132)',
-                data: [0, 10, 5, 2, 20, 30, 45]
+                data: [0, 10, 5, 2, 20, 30, 45, 30, 25, 8, 10, 40]
             }]
         },
     
         // Configuration options go here
-        options: {}
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        major: {
+                            beginAtZero: true
+                        }
+                    }
+                }]
+            }
+        }
     });
+
+    function gHandler(value){
+        graphHandler(value, verticalSpan, graphSlider, graphContainer, chart)
+    }
+
+    verticalSlider.addEventListener('input', (e) => gHandler(e.target.value));
 
     
 
